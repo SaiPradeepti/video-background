9227378b89e96b69d27d42358a270d25f953c47e
SaiPradeepti <saipradeepti14@gmail.com>
2021-01-24 17:34:32 +0530

    Adding Styles

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-document.querySelector('video').playbackRate = 0.6;
+document.querySelector('video').playbackRate = 1;
+
+ const menuToggle = document.querySelector('.toggle');
+      const showcase = document.querySelector('.showcase');
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        showcase.classList.toggle('active');
+      })
